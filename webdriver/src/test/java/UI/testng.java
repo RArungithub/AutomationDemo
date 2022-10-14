ab43f8b64a07e881b6d746bd279012d4a2e2b277
Arun <Arun@Arun>
2022-10-14 10:57:57 +0530

    First Commit

---
+++ webdriver/src/test/java/UI/testng.java
@@ -0,0 +1,24 @@
+package UI;
+
+import org.testng.annotations.Test;
+
+public class testng {
+    @Test
+    
+	public void login() 
+	{
+	System.out.println("log in successfull");
+	}
+    
+    @Test
+    public void logout()
+    {
+    System.out.println("log in unsuccessful");
+    
+    
+    
+    
+	
+
+}
+}
